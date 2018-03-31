f332eb24fc69e97b12840901fa7fd3fd7d5798e3
eight9080 <eight9080@gmail.com>
2018-04-01 01:52:43 +0300

    reactive service

--- src/main/java/ro/dg/springbootapp/springbootapp/controller/HomeController.java
+++ src/main/java/ro/dg/springbootapp/springbootapp/controller/HomeController.java
@@ -3,6 +3,7 @@ package ro.dg.springbootapp.springbootapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ro.dg.springbootapp.springbootapp.domain.model.Image;
 
 @RestController
 public class HomeController {
@@ -11,4 +12,5 @@ public class HomeController {
     public String greeting(@RequestParam(required = false, defaultValue = "")String name){
         return name.equals("") ?"Hey!" :"Hey, "+name+"!";
     }
+
 }
